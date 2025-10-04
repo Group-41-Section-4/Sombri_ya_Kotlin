cb493f4aff1082b97e56998a029228d68286b86d
ju4ndiegos <jd.sanchez2@uniandes.edu.co>
2025-10-04 13:59:55 -0500

    Fix: Correct station API endpoint
    The `getStations` endpoint was changed from "stations" to "stations/nearby".

--- app/src/main/java/com/example/sombriyakotlin/data/api/StationApi.kt
+++ app/src/main/java/com/example/sombriyakotlin/data/api/StationApi.kt
@@ -7,7 +7,7 @@ import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface StationApi {
-    @POST("stations")
+    @POST("statiosn/nearby")
     suspend fun getStations(@Body queryStationDto: QueryLocalizationDto): List<StationDto>
 
 }
