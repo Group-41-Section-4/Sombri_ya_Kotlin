a3169bb248f17f9f4f06aac80c22c1d66a03c0ff
jaime <je.morenoj1@uniandes.edu.co>
2025-10-23 11:56:12 -0500

    ask For notifications

--- app/src/main/java/com/example/sombriyakotlin/SombriYaKotlinApp.kt
+++ app/src/main/java/com/example/sombriyakotlin/SombriYaKotlinApp.kt
@@ -9,6 +9,9 @@ import android.util.Log
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import com.example.sombriyakotlin.data.location.GPSManager
 import dagger.hilt.android.HiltAndroidApp
 import androidx.work.Constraints
@@ -35,6 +38,22 @@ class SombriYaKotlinApp : Application(), Configuration.Provider {
         scheduleWeatherCheck(this)
         Log.d("EMPIEZAAPP", "Se llamo todo correctamente")
     }
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "default_channel_id"
+            val channelName = "Notificaciones generales"
+            val channelDescription = "Canal para las notificaciones del sistema"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+
+            val channel = NotificationChannel(channelId, channelName, importance).apply {
+                description = channelDescription
+            }
+
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(channel)
+        }
+    }
+}
 
     override val workManagerConfiguration: Configuration
         get() = Configuration.Builder()
