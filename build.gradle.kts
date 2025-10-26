e7c20bda11c142826f20c398dad5b44436094e2f
ju4ndiegos <jd.sanchez2@uniandes.edu.co>
2025-10-26 14:06:31 -0500

    feat: Integrate Firebase via google-services plugin
    - Add the `com.google.gms.google-services` Gradle plugin to the project build file.
    - Move the `google-services.json` file to the `app/` directory to enable Firebase services.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,8 @@ plugins {
     alias(libs.plugins.secrets.gradle.plugin) apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false // <-- Incompatible version
-    }
+    id("com.google.gms.google-services") version "4.4.4" apply false
+}
 
 buildscript{
     dependencies {
