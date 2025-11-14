3dd8776480c91c4e1fe34073a98e4f637ccc0543
ju4ndiegos <jd.sanchez2@uniandes.edu.co>
2025-11-13 20:26:14 -0500

    chore: Remove google-services plugin
    Removes the `com.google.gms.google-services` plugin from the app-level `build.gradle.kts` file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
-    id("com.google.gms.google-services")
 
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
     id("com.google.dagger.hilt.android")
